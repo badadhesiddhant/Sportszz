21976fe1ca89f9b94a3242a2b869a7717d94921b
Akash <aakash7536@gmail.com>
2024-10-03 11:55:53 +0530

    Updated changes

--- server/server.js
+++ server/server.js
@@ -4,30 +4,32 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const protectedRoutes = require("./routes/protectedRoutes");
 const updateRoutes = require("./routes/updateRoutes");
-const tournamentRoutes = require('./routes/tournamentRoutes');
+const tournamentRoutes = require("./routes/tournamentRoutes");
 require("dotenv").config();
 
 const app = express();
-const path = require('path');
+const path = require("path");
 app.use(express.json());
 app.use(cors());
 
+const dbURI = process.env.MONGO_URI;
 
-// Connect to MongoDB
+if (!dbURI) {
+  console.error("MONGO_URI is not defined");
+  process.exit(1);
+}
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Use routes
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
 app.use("/api", protectedRoutes);
-app.use('/api', updateRoutes);
-app.use('/api', tournamentRoutes);
+app.use("/api", updateRoutes);
+app.use("/api", tournamentRoutes);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
